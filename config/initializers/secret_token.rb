c891cb5a17973ffa2572f1a3fe9a7c77dce403d4
Alex <alex.korban@gmail.com>
2011-03-17 17:41:07 +1300

    Fix: modified secret because Devise requires it due to some changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OnlineChecklistsApp::Application.config.secret_token = '88782bfa20cfc074c366f754ec87d7867ab14829ec1ec1dcad664bf93ac7f14fceca185f58c3087e7b49de99c5155dd1b41191fcbae6a692c91a6564577ecefa'
+OnlineChecklistsApp::Application.config.secret_token = '88782bfa20cfc174c366f754ec87d7867ab14829ec1ec1dcad664bf93ac7f14fceca185f58c3087e7b49de99c5155dd1b41191fcbae6a692c91a6564577ecefa'
